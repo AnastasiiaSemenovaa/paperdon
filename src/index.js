fb4e20c220049b287a3e453e83c05df8618f673b
ivanbatarin <ivan.batarin@boosta.co>
2023-05-04 14:35:22 +0200

    fixing v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './style.scss';
 
-console.log('Webpack starter with SCSS and dev server');
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    let burger = document.querySelector('.burger');
+    let topNav = document.querySelector('.header__nav');
+
+    if (burger && topNav) {
+        burger.addEventListener('click', function () {
+
+            this.classList.toggle('open');
+            topNav.classList.toggle('open');
+
+        });
+    }
+
+
+    function changeLoginBtn() {
+        let loginBtn = document.querySelector('.header__log-link');
+        if (loginBtn) {
+            if (window.innerWidth <= 767) {
+                loginBtn.innerHTML = 'Log in';
+            } else {
+                loginBtn.innerHTML = 'Log in / Sign up';
+            }
+        }
+    }
+
+    changeLoginBtn();
+    window.addEventListener('resize', function () {
+        changeLoginBtn()
+    });
+
+});
